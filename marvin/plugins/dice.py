6cb655a62e9b813217214cbad888f1f9f49aa555
lexifdev <sl@lxf.kr>
2015-08-24 22:24:07 +0900

    42 선호 성향

--- marvin/plugins/dice.py
+++ marvin/plugins/dice.py
@@ -3,6 +3,13 @@ import random
 from .base import BaseHandler
 
 
+ANSWER = 42
+
+def got_lucky():
+    luck = random.randint(1, 100)
+    return luck < 30
+
+
 class DiceHandler(BaseHandler):
     @asyncio.coroutine
     def on_message(self, message):
@@ -17,11 +24,19 @@ class DiceHandler(BaseHandler):
                 if args_length == 2:
                     arg = int(args[1])
                     result = random.randint(1, arg)
+
+                    if arg >= ANSWER and got_lucky():
+                        result = ANSWER
+
                     yield from self.send_text(channel, str(result))
                 elif args_length == 3:
                     arg1 = int(args[1])
                     arg2 = int(args[2])
                     result = random.randint(arg1, arg2)
+
+                    if arg1 < ANSWER < arg2 and got_lucky():
+                        result = ANSWER
+
                     yield from self.send_text(channel, str(result))
                 else:
                     raise ValueError
