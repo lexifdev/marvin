795d15b8d40b7fb7610f129111663da32dc8fd2b
lexifdev <sl@lxf.kr>
2015-08-25 16:59:51 +0900

    동의 구하는 질문 추가

--- marvin/plugins/agree.py
+++ marvin/plugins/agree.py
@@ -9,6 +9,6 @@ class AgreeHandler(BaseHandler):
         text = message.get('text', '').lower()
         channel = message['channel']
 
-        if text.endswith('그렇지?'):
+        if text.endswith('그렇지?') or text.endswith('그러니?'):
             result = random.choice(['그럼요', '물론이죠', '아닐걸요', '그럴리가요', '전 모르겠어요'])
             yield from self.send_text(channel, result)
