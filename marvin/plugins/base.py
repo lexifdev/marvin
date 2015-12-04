2d885fc688bf66f3331ecf39c285a7b801e9d40f
lexifdev <sl@lxf.kr>
2015-12-05 01:07:34 +0900

    slack api client 추가

--- marvin/plugins/base.py
+++ marvin/plugins/base.py
@@ -8,6 +8,10 @@ class BaseHandler(object):
     def on_message(self, message):
         pass
 
+    @property
+    def api(self):
+        return self._bot.api
+
     @asyncio.coroutine
     def send_text(self, channel, text):
         yield from self._bot.send_text(channel, text)
